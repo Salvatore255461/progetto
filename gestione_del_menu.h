d66fd3e0814ba88754bf648bf525cc17386dcd8b
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-22 17:45:32 +0100

    Create gestione_del_menu.h

---
+++ gestione_del_menu.h
@@ -0,0 +1,13 @@
+#ifndef GESTIONE_MENU_H_INCLUDED
+#define GESTIONE_MENU_H_INCLUDED
+
+// Mostra le opzioni e restituisce la scelta dell'utente
+int mostra_menu_principale();
+
+// Legge il file regolamento.txt e lo stampa a video
+void visualizza_regolamento();
+
+// Chiede all'utente la difficoltà
+int scegli_difficolta();
+
+#endif // GESTIONE_MENU_H_INCLUDED
