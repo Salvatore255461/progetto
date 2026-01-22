f1be638edaf4abf760fa6afb031fe5b6f14b550f
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-22 17:42:19 +0100

    Create gestione_del_gioco.h

---
+++ gestione_del_gioco.h
@@ -0,0 +1,6 @@
+#ifndef GESTINE_DEL_GIOCO_H_INCLUDED
+#define GESTINE_DEL_GIOCO_H_INCLUDED
+
+void gioca_partita(char *username);    // Prende in ingresso il nome dell'utente loggato.
+
+#endif // GESTINE_DEL_GIOCO_H_INCLUDED
