a6975934fa702a6c95253555158a380a29b6c68c
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-22 17:43:02 +0100

    Update gestione_file_di_testo.h

--- gestione_file_di_testo.h
+++ gestione_file_di_testo.h
@@ -1,6 +1,8 @@
 #ifndef GESTIONE_FILE_DI_TESTO_H_INCLUDED
 #define GESTIONE_FILE_DI_TESTO_H_INCLUDED
 
-int estrai_parole_dal_file(const char *nome_file,int lunghezza_cercata,int max_parole_da_trovare,char *matrice_parole[],char *matrice_def[]);      // apre il file e cerca le parole della lunghezza giusta
-                                                                                                                                                    // Restituisce il numero di parole effettivamente trovate.
+// Funzione che apre il file, cerca le parole della lunghezza giusta e riempie le matrici.
+// Restituisce il numero di parole effettivamente trovate.
+int estrai_parole_dal_file(const char *nome_file,int lunghezza_cercata,int max_parole_da_trovare,char *matrice_parole[],char *matrice_def[]);
+
 #endif // GESTIONE_FILE_DI_TESTO_H_INCLUDED
