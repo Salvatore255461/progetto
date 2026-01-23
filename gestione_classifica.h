6f7db8a316cf72783d7d779070b178cd281f6c9b
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-23 10:56:19 +0100

    Update gestione_classifica.h

--- gestione_classifica.h
+++ gestione_classifica.h
@@ -1,12 +1,14 @@
 #ifndef GESTIONE_CLASSIFICA_H_INCLUDED
 #define GESTIONE_CLASSIFICA_H_INCLUDED
 
-// Definiamo qui la struttura, così possiamo usarla anche altrove se serve
-typedef struct {
+typedef struct Giocatore {
     char username[50];
     char password[50];
     int punteggio;
+
+    struct Giocatore *next; // PUNTATORE AL PROSSIMO NODO
 } Giocatore;
 
-void mostra_classifica_ordinata();    // Funzione che legge, ordina e stampa la classifica
+void mostra_classifica_ordinata();            // Funzione principale che legge, ordina e stampa la classifica usando le liste
+
 #endif // GESTIONE_CLASSIFICA_H_INCLUDED
