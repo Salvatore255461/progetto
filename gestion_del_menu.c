3938c45565216e193efd37e20edc43c675cb705e
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-23 10:54:02 +0100

    Refactor main menu function to include user state

--- gestion_del_menu.c
+++ gestion_del_menu.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "gestione_menu.h"
-#include "gestione_classifica.h"
 
 #define FILE_REGOLAMENTO "regolamento.txt"
-#define FILE_UTENTI "utenti.txt"
-#define FILE_TEMP "temp_utenti.txt"
 
-int mostra_menu_principale() {                      // visualizzazione del menù iniziale
+int mostra_menu_principale(int utente_loggato, char *nome_utente) {
     int scelta;
-    printf("\n=================================\n");
+
+    printf("\n\n");
+    printf("=================================\n");
     printf("      PAROLE CROCIATE - MENU     \n");
     printf("=================================\n");
-    printf("1. Accedi / Registrati e GIOCA\n");
+
+    if (utente_loggato == 0) {
+        // CASO A: Nessuno loggato
+        printf("1. Accedi / Registrati e GIOCA\n");
+    } else {
+        // CASO B: Utente loggato
+        printf("1. GIOCA (Utente: %s)\n", nome_utente);
+    }
+
     printf("2. Regolamento\n");
     printf("3. Classifica\n");
+
+    if (utente_loggato == 1) {                        // Mostriamo questa opzione SOLO se sei loggato
+        printf("4. Logout / Cambia Utente\n");
+    }
+
     printf("0. Esci\n");
     printf("---------------------------------\n");
     printf("Inserisci scelta: ");
@@ -29,7 +40,6 @@ int mostra_menu_principale() {                      // visualizzazione del menù
 
 int scegli_difficolta() {
     int liv;
-
     printf("\n=== SCEGLI LA DIFFICOLTA' ===\n");
     printf("1. FACILE    (Meta' lettere visibili) [Vittoria: +10 pt]\n");
     printf("2. NORMALE   (3 lettere visibili)     [Vittoria: +15 pt]\n");
@@ -48,7 +58,7 @@ void visualizza_regolamento() {
     FILE *fp = fopen(FILE_REGOLAMENTO, "r");
     char buffer[255];
 
-    printf("\n=== REGOLAMENTO DI GIOCO ===\n\n");
+    printf("\n\n=== REGOLAMENTO DI GIOCO ===\n\n");
 
     if (fp == NULL) {
         printf("Errore: Impossibile trovare il file '%s'.\n", FILE_REGOLAMENTO);
