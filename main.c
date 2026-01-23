91cdd9bb940fdf092364f73b04902edd2644d555
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-23 14:21:38 +0100

    Enhance user login and account management features

--- main.c
+++ main.c
@@ -11,19 +11,56 @@ int main() {
     char current_user[50] = "";
     int utente_loggato = 0;
 
+    int scelta_pre_gioco;
+
     while (1) {
         scelta = mostra_menu_principale(utente_loggato, current_user);
 
         switch (scelta) {
             case 1: // LOGIN / GIOCA
+                // A. Se non sono loggato, provo a fare login
                 if (utente_loggato == 0) {
-                                                    // Se NON sono loggato, mostro la schermata di login/registrazione
                     if (menu_autenticazione(current_user) == 1) {
                         utente_loggato = 1;
+                    }
+                }
+
+                // B. Se ora sono loggato
+                if (utente_loggato == 1) {
+                    // --- SOTTO-MENU PRE-PARTITA ---
+                    printf("\n-----------------------------------\n");
+                    printf("Ciao %s, cosa vuoi fare?\n", current_user);
+                    printf("1. Procedi al Gioco (Scegli difficolta')\n");
+                    printf("2. Elimina questo Account\n");
+                    printf("0. Annulla e torna al Menu\n");
+                    printf("-----------------------------------\n");
+                    printf("Scelta: ");
+
+                    if (scanf("%d", &scelta_pre_gioco) != 1) {
+                        while(getchar() != '\n'); // Pulisci buffer se inserisce lettere
+                        scelta_pre_gioco = 0;
+                    }
+
+                    if (scelta_pre_gioco == 1) {
+                        // AVVIA IL GIOCO
                         gioca_partita(current_user);
                     }
-                } else {                                // Se SONO GIA' loggato, gioco direttamente
-                    gioca_partita(current_user);
+                    else if (scelta_pre_gioco == 2) {
+                        // AVVIA ELIMINAZIONE
+                        if (procedura_eliminazione_account(current_user) == 1) {
+                            // Se eliminato, logout forzato
+                            utente_loggato = 0;
+                            strcpy(current_user, "");
+                            printf("\n(Sei stato disconnesso automaticamente)\n");
+                            // Pausa per leggere
+                            printf("Premi INVIO per tornare al menu...");
+                            while(getchar() != '\n'); getchar();
+                        }
+                    }
+                    else {
+                        printf("\nRitorno al menu principale...\n");
+                    }
+                    // ------------------------------
                 }
                 break;
 
@@ -35,21 +72,30 @@ int main() {
                 mostra_classifica_ordinata();
                 break;
 
-            case 4: // LOGOUT / CAMBIA UTENTE
+            case 4: // LOGOUT (Visibile solo se loggato)
                 if (utente_loggato == 1) {
-                    printf("\n>> Logout effettuato con successo. A presto %s!\n", current_user);
-
+                    printf("\n>> Logout effettuato. A presto %s!\n", current_user);
                     utente_loggato = 0;
                     strcpy(current_user, "");
-
-                    printf("Premi INVIO per continuare...");
-                    while(getchar() != '\n'); getchar();
                 } else {
-                    printf("Opzione non valida (non sei loggato).\n");
+                    printf("Opzione non valida.\n");
                 }
                 break;
 
-            case 0: // ESCI DAL PROGRAMMA
+            case 5: // ELIMINA ACCOUNT
+                if (utente_loggato == 1) {
+                    if (procedura_eliminazione_account(current_user) == 1) {
+                        utente_loggato = 0;
+                        strcpy(current_user, "");
+                        printf("\n(Sei stato disconnesso automaticamente)\n");
+                        while(getchar() != '\n'); getchar();
+                    }
+                } else {
+                    printf("Opzione non valida.\n");
+                }
+                break;
+
+            case 0:
                 printf("Arrivederci!\n");
                 return 0;
 
@@ -59,3 +105,4 @@ int main() {
     }
     return 0;
 }
+
