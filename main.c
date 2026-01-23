f32c4ebe687d82d7332980382b92d50240bbbe40
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-23 10:55:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "gestione_menu.h"
 #include "gestione_del_gioco.h"
@@ -11,21 +12,22 @@ int main() {
     int utente_loggato = 0;
 
     while (1) {
-        scelta = mostra_menu_principale();
+        scelta = mostra_menu_principale(utente_loggato, current_user);
 
         switch (scelta) {
-            case 1:
+            case 1: // LOGIN / GIOCA
                 if (utente_loggato == 0) {
+                                                    // Se NON sono loggato, mostro la schermata di login/registrazione
                     if (menu_autenticazione(current_user) == 1) {
                         utente_loggato = 1;
                         gioca_partita(current_user);
                     }
-                } else {
+                } else {                                // Se SONO GIA' loggato, gioco direttamente
                     gioca_partita(current_user);
                 }
                 break;
 
-            case 2:
+            case 2: // REGOLAMENTO
                 visualizza_regolamento();
                 break;
 
@@ -33,7 +35,21 @@ int main() {
                 mostra_classifica_ordinata();
                 break;
 
-            case 0:
+            case 4: // LOGOUT / CAMBIA UTENTE
+                if (utente_loggato == 1) {
+                    printf("\n>> Logout effettuato con successo. A presto %s!\n", current_user);
+
+                    utente_loggato = 0;
+                    strcpy(current_user, "");
+
+                    printf("Premi INVIO per continuare...");
+                    while(getchar() != '\n'); getchar();
+                } else {
+                    printf("Opzione non valida (non sei loggato).\n");
+                }
+                break;
+
+            case 0: // ESCI DAL PROGRAMMA
                 printf("Arrivederci!\n");
                 return 0;
 
