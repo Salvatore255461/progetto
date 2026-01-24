c76dc8aec429de4569927b6c7f9aaf5aa91f8bbe
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-24 09:03:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,66 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "gestione_menu.h"
-#include "gestione_del_gioco.h"
-#include "gestione_classifica.h"
-#include "gestione_utenti.h"
+#include "interfaccia.h"
 
 int main() {
     int scelta;
     char current_user[50] = "";
     int utente_loggato = 0;
 
-    int scelta_pre_gioco;
-
     while (1) {
         scelta = mostra_menu_principale(utente_loggato, current_user);
 
         switch (scelta) {
             case 1: // LOGIN / GIOCA
-                // A. Se non sono loggato, provo a fare login
+                // A. Se non sono loggato, faccio login
                 if (utente_loggato == 0) {
                     if (menu_autenticazione(current_user) == 1) {
                         utente_loggato = 1;
-                    }
-                }
-
-                // B. Se ora sono loggato
-                if (utente_loggato == 1) {
-                    // --- SOTTO-MENU PRE-PARTITA ---
-                    printf("\n-----------------------------------\n");
-                    printf("Ciao %s, cosa vuoi fare?\n", current_user);
-                    printf("1. Procedi al Gioco (Scegli difficolta')\n");
-                    printf("2. Elimina questo Account\n");
-                    printf("0. Annulla e torna al Menu\n");
-                    printf("-----------------------------------\n");
-                    printf("Scelta: ");
-
-                    if (scanf("%d", &scelta_pre_gioco) != 1) {
-                        while(getchar() != '\n'); // Pulisci buffer se inserisce lettere
-                        scelta_pre_gioco = 0;
-                    }
-
-                    if (scelta_pre_gioco == 1) {
-                        // AVVIA IL GIOCO
+                        // Appena loggato, vado SUBITO alla scelta difficoltà
                         gioca_partita(current_user);
                     }
-                    else if (scelta_pre_gioco == 2) {
-                        // AVVIA ELIMINAZIONE
-                        if (procedura_eliminazione_account(current_user) == 1) {
-                            // Se eliminato, logout forzato
-                            utente_loggato = 0;
-                            strcpy(current_user, "");
-                            printf("\n(Sei stato disconnesso automaticamente)\n");
-                            // Pausa per leggere
-                            printf("Premi INVIO per tornare al menu...");
-                            while(getchar() != '\n'); getchar();
-                        }
-                    }
-                    else {
-                        printf("\nRitorno al menu principale...\n");
-                    }
-                    // ------------------------------
+                }
+                // B. Se ero già loggato, vado SUBITO alla scelta difficoltà
+                else {
+                    gioca_partita(current_user);
                 }
                 break;
 
@@ -72,7 +35,7 @@ int main() {
                 mostra_classifica_ordinata();
                 break;
 
-            case 4: // LOGOUT (Visibile solo se loggato)
+            case 4: // LOGOUT
                 if (utente_loggato == 1) {
                     printf("\n>> Logout effettuato. A presto %s!\n", current_user);
                     utente_loggato = 0;
@@ -105,4 +68,3 @@ int main() {
     }
     return 0;
 }
-
