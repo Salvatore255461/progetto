5e37db062560be6ab717f8ef0aaaa7fad89e07c4
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-24 09:22:57 +0100

    eliminazione file

--- gestione_del_menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef GESTIONE_MENU_H_INCLUDED
-#define GESTIONE_MENU_H_INCLUDED
-
-int mostra_menu_principale(int utente_loggato, char *nome_utente);    // MODIFICA:
-
-void visualizza_regolamento();
-int scegli_difficolta();
-
-#endif // GESTIONE_MENU_H_INCLUDED
--- gestione_file_di_testo.h
+++
@@ -1,8 +0,0 @@
-#ifndef GESTIONE_FILE_DI_TESTO_H_INCLUDED
-#define GESTIONE_FILE_DI_TESTO_H_INCLUDED
-
-// Funzione che apre il file, cerca le parole della lunghezza giusta e riempie le matrici.
-// Restituisce il numero di parole effettivamente trovate.
-int estrai_parole_dal_file(const char *nome_file,int lunghezza_cercata,int max_parole_da_trovare,char *matrice_parole[],char *matrice_def[]);
-
-#endif // GESTIONE_FILE_DI_TESTO_H_INCLUDED
