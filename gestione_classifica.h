a2ebbcf0c4f04b88b70b42aa0579b527316b334a
Salvatore255461 <dnisvt04s29c352p@studenti.unical.it>
2026-01-24 09:12:36 +0100

    Update gestione_classifica.h

--- gestione_classifica.h
+++ gestione_classifica.h
@@ -1,14 +1,1 @@
-#ifndef GESTIONE_CLASSIFICA_H_INCLUDED
-#define GESTIONE_CLASSIFICA_H_INCLUDED
 
-typedef struct Giocatore {
-    char username[50];
-    char password[50];
-    int punteggio;
-
-    struct Giocatore *next; // PUNTATORE AL PROSSIMO NODO
-} Giocatore;
-
-void mostra_classifica_ordinata();            // Funzione principale che legge, ordina e stampa la classifica usando le liste
-
-#endif // GESTIONE_CLASSIFICA_H_INCLUDED
